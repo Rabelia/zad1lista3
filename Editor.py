597f19cef021257a0ed7f66477d67830a19bb155
Rabelia <105874122+Rabelia@users.noreply.github.com>
2022-05-19 18:42:33 +0200

    Add files via upload

---
+++ Editor.py
@@ -0,0 +1,30 @@
+from classes.User import *
+from classes.Exceptions import *
+
+
+class Editor:
+    def __init__(self):
+        self.user-name = None
+        # self.options = {"a": self.login(), "b": self.test(), "c": self.change(), "d": self.quit()}
+
+    def login(self, user_list):
+        username = input("Podaj nazwę użtykownika: ")
+        password = input("Podaj hasło: ")
+        user_list.login(username, password)
+
+    def is_permitted(self):
+        pass
+
+    def test(self):
+        pass
+
+    def change(self):
+        pass
+
+    def quit(self):
+        pass
+
+    def run(self):
+        # print("Podaj Klucz:")
+        # key = input()
+        pass
